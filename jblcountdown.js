c57417be22a00905a4f49db3db4e64c6e89ca1ad
Joel Lisenby <joel.lisenby@gmail.com>
2016-11-18 09:57:40 -0800

    Update jblcountdown.js

--- jblcountdown.js
+++ jblcountdown.js
@@ -35,10 +35,10 @@ var jblcountdown = function( id, begin, end ) {
 		var html = '';
 		
 		if( jt.timeleft > 0 && jt.now.getTime() > jt.begin.getTime() ) {
-			html += jt.days > 0 ? '<div class="days"><span class="time">'+ jt.days +'</span><span class="label">Day'+ ( jt.days > 1 || jt.days == 0 ? 's' : '' ) +'</span></div>' : '';
-			html += jt.hours > 0 ? '<div class="hours"><span class="time">'+ jt.hours +'</span><span class="label">Hour'+ ( jt.hours > 1 || jt.hours == 0 ? 's' : '' ) +'</span></div>' : '';
-			html += jt.minutes > 0 ? '<div class="minutes"><span class="time">'+ jt.minutes +'</span><span class="label">Minute'+ ( jt.minutes > 1 || jt.minutes == 0 ? 's' : '' ) +'</span></div>' : '';
-			html += jt.seconds >= 0 ? '<div class="seconds"><span class="time">'+ jt.seconds +'</span><span class="label">Second'+ ( jt.seconds > 1 || jt.seconds == 0 ? 's' : '' ) +'</span></div>' : '';
+			html += jt.days > 0 ? '<div class="days"><span class="time">'+ jt.days +'</span><span class="label">Day'+ ( jt.s( jt.days ) ? 's' : '' ) +'</span></div>' : '';
+			html += jt.hours > 0 ? '<div class="hours"><span class="time">'+ jt.hours +'</span><span class="label">Hour'+ ( jt.s( jt.hours ) ? 's' : '' ) +'</span></div>' : '';
+			html += jt.minutes > 0 ? '<div class="minutes"><span class="time">'+ jt.minutes +'</span><span class="label">Minute'+ ( jt.s( jt.minutes ) ? 's' : '' ) +'</span></div>' : '';
+			html += jt.seconds >= 0 ? '<div class="seconds"><span class="time">'+ jt.seconds +'</span><span class="label">Second'+ ( jt.s( jt.seconds ) ? 's' : '' ) +'</span></div>' : '';
 		} else if( jt.now.getTime() < jt.begin.getTime() && jt.now.getTime() <= jt.end.getTime() ) {
 			html += '<div>Available Soon!</div>';
 		} else {
@@ -46,6 +46,10 @@ var jblcountdown = function( id, begin, end ) {
 		}
 		
 		jt.obj.innerHTML = html;
+	};
+	
+	jt.s = function(n) {
+		return n > 1 || n == 0 ? true : false;
 	};
 	
 	jt.start();
